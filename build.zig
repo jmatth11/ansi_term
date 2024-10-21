c86c94b7200888d62df25d32910c6385f12d5749
Joshua Matthews <jmatth93@gmail.com>
2024-10-20 22:56:19 -0500

    add as module

--- build.zig
+++ build.zig
@@ -3,14 +3,20 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const root_file = b.path("src/ansi.zig");
     // library setup
     const lib = b.addStaticLibrary(.{
         .name = "ansi_term",
-        .root_source_file = b.path("src/ansi.zig"),
+        .root_source_file = root_file,
         .target = target,
         .optimize = optimize,
     });
     b.installArtifact(lib);
+
+    // add as module to be imported
+    _ = b.addModule("ansi", .{
+        .root_source_file = root_file,
+    });
 
     // unit test setup
     const lib_unit_tests = b.addTest(.{
